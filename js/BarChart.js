3ad3b672345750b2d95f837b804e9019c07031b7
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-03-30 18:42:13 -0400

    Append rect bars to bar-chart svg

--- js/BarChart.js
+++ js/BarChart.js
@@ -2,10 +2,27 @@ var dataUrl = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceDa
 
 d3.json(dataUrl, function(json) {
 	console.log(json);
-	var data = json.data;
+	var dataSet = json.data;
 	var dataDescription = json.description;
 
 	d3.select(".bar-chart-title").text("Bar Chart of US Gross Domestic Project");
 	d3.select(".bar-chart-description").text(dataDescription);
+
+	d3.select(".bar-chart")
+		.attr("width", "100%")
+		.attr("height", "100%")
+		.attr("viewBox", "0 0 500 500");
+
+	d3.select(".bar-chart")
+		.selectAll("rect")
+		.data(dataSet)
+		.enter()
+		.append("rect")
+		.attr("class", "bar")
+			.attr("height", function(d, i) { return d[1]; })
+			.attr("width", "100")
+			.attr("x", function(d, i) { return i; })
+			.attr("y", "50");
+
 });
 
