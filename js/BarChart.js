84ae2b54bb41b3b4da16c537fe12d5544461708d
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-04-01 17:54:28 -0400

    Move X-Axis label to the bottom of bar-chart

--- js/BarChart.js
+++ js/BarChart.js
@@ -4,17 +4,20 @@ d3.json(dataUrl, function(json) {
 	console.log(json);
 	var dataSet = json.data;
 	var dataDescription = json.description;
+	var height = 400;
+	var width = 400;
 
 	d3.select(".bar-chart-title").text("Bar Chart of US Gross Domestic Project");
 	d3.select(".bar-chart-description").text(dataDescription);
 
-	d3.select(".bar-chart")
-		.attr("width", "400")
-		.attr("height", "400")
-		.attr("viewBox", "0 0 400 500");
+	var svg = d3.select(".bar-chart")
+		.attr("width", height)
+		.attr("height", width)
+		.attr("viewBox", "0 0 400 450");
 
-	d3.select(".bar-chart")
-		.selectAll("rect")
+	var barScale = d3.scaleLinear().domain([1900, 2015]).range([0, 25000]);
+
+	var rects = svg.selectAll("rect")
 		.data(dataSet)
 		.enter()
 		.append("rect")
@@ -22,7 +25,19 @@ d3.json(dataUrl, function(json) {
 			.attr("height", function(d, i) { return d[1] / 40; })
 			.attr("width", "150")
 			.attr("x", function(d, i) { return i; })
-			.attr("y", function(d,i) { return 500 - (d[1] / 40); });
+			.attr("y", function(d,i) { return height - (d[1] / 40); });
+
+	var xAxis = d3.axisBottom()
+		.scale(barScale);
+
+	svg.append('g')
+		.attr("transform", "translate(0," + 415 + ")")
+		.call(xAxis);
+
+	svg.append('text')
+		.style("text-anchor", "middle")
+		.attr("transform", "translate(0," + 410 + ")")
+		.text("Date");
 
 });
 
